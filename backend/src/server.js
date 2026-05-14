117ce98afacb107690310da30a992a17fc52b9e1
anshsingh1032 <thansh1032@gmail.com>
2026-05-15 03:03:01 +0530

    connect db and add some utilities

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,27 @@ import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
+import connectDB from "./db/index.js"
 
 dotenv.config()
 
 const app = express()
 const PORT=process.env.PORT
 
+app.use(express.json({
+    limit:"16kb"
+}))
+
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
-app.listen(PORT,()=>console.log(`server is running on port ${PORT }`))
+connectDB()
+.then(()=>{
+    app.listen(PORT || 8000 ,()=>{
+        console.log(`server is listening on ${PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed !!",err);
+    
+})
