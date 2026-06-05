9d63ca363be5579e682bddfb21bc378ce31844e6
anshsingh1032 <thansh1032@gmail.com>
2026-06-05 12:51:12 +0530

    add message routes

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,16 @@
 import express from "express"
+import { protectRoute } from "../middlewares/auth.middleware.js"
+import { getAllContacts, getChatPartners, getMessagesByUserId , sendMessage} from "../controllers/message.controller.js"
+import { upload } from "../middlewares/multer.middleware.js"
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js"
 
 const router = express.Router()
 
-router.get("/send",(req,res)=>{
-    res.send("Send message endpoint")
-})
+router.use(arcjetProtection,protectRoute)
+
+router.route("/contacts").get(getAllContacts)
+router.route("/chat").get(getChatPartners)
+router.route("/:id").get(getMessagesByUserId)
+router.route("/send/:id").post(upload.single("image"),sendMessage)
 
 export default router
